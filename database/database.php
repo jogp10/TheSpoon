896371074cc880aea93fb6d466f9b8a202118cdb
jogp10 <jogp10@gmail.com>
2022-05-06 19:48:13 +0100

    fix

--- database/database.php
+++ database/database.php
@@ -11,7 +11,7 @@ class databaseManagement {
      * Initialize database
      */
     public function __construct() {
-        $this->db = new PDO('sqlite:theSpoon.db');
+        $this->db = new PDO('sqlite:database/theSpoon.db');
         if ($this->db == null)
           throw new Exception('Database not initialized');
     }
@@ -51,10 +51,16 @@ class databaseManagement {
      */
     public function insertAddress($state, $city, $street, $postalCode) {
         if ($state == NULL || $city == NULL || $street == NULL || $postalCode == NULL) return -1;
-        $stmt1 = $this->db->prepare('SELECT MAX(idAddress) FROM Address');
-        $stmt1->execute();
-        $idAddress = $stmt1->fetch() + 1;
-        echo $idAddress;
+        $stmt = $this->db->prepare('SELECT * FROM sqlite_master');
+        if(!$stmt) {
+            echo "Prepare failed: (". $this->db->errorCode().") ".$this->db->errorInfo()."<br>";
+            foreach($this->db->errorInfo() as $error){
+                echo ".$error ";
+            }
+            return -1;
+        }
+        $stmt->execute();
+        $idAddress = $stmt->fetchAll();
         $stmt = $this->db->prepare("INSERT INTO Address(idAddress, Street, City, State, PostalCode) values ('$idAddress', '$street', '$city', '$state', '$postalCode')");
         $stmt->execute();
         return $idAddress;
