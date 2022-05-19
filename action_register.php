5ff0c7b1c885c74ce61e29868f3158f16aca0812
jogp10 <jogp10@gmail.com>
2022-05-19 19:41:26 +0100

    db change

--- action_register.php
+++ action_register.php
@@ -8,22 +8,16 @@
 
   $db = getDatabaseConnection();
 
-  if ($_POST['password']!=$_POST['password-repeat']) {
-    header('Location: register.php');
-    //Error message displaying different passwords;
-  }
-  else {
-    $user = User::addUser($db, $_POST['email'], $_POST['password'], (int)$_POST['phone'], $_POST['name'], (bool)$_POST['restaurant-owner'], $_POST['street'], $_POST['city'], $_POST['state'], (int)$_POST['postal-code']);
-        
-    if ($user != null) {
-      $_SESSION['id'] = $user->idUser;
-      $_SESSION['name'] = $user->name;
-      $_SESSION['email'] = $user->email;
-      header('Location: /');
-    } else {
-      header('Location: register.php' . $_SERVER['HTTP_REFERER']);
-      // Error message displaying user already exists
-    }
+  $user = User::addUser($db, $_POST['email'], $_POST['password'], (int)$_POST['phone'], $_POST['name'], (bool)$_POST['restaurant-owner'], $_POST['street'], $_POST['city'], $_POST['state'], (int)$_POST['postal-code']);
+      
+  if ($user != null) {
+    $_SESSION['id'] = $user->idUser;
+    $_SESSION['name'] = $user->name;
+    $_SESSION['email'] = $user->email;
+    header('Location: /');
+  } else {
+    header('Location: /index.php' . $_SERVER['HTTP_REFERER']);
+    // Error message displaying user already exists
   }
 
 ?>
