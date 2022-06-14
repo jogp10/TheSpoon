0f80af5946d1f92a5d5e83e34b9943cd08917a39
Cavaleiro132 <vcavaleiro75@gmail.com>
2022-06-14 23:40:03 +0100

    Minor change

--- actions/action_profile.php
+++ actions/action_profile.php
@@ -6,13 +6,26 @@
 
   require_once('../database/connection.php');
   require_once('../database/user.class.php');
+  require_once('../utils/string_manip.php');
 
   $db = getDatabaseConnection();
 
   $user = User::getUser($db, $session->getId());
 
-  $user->updateUser($db, $_POST['name'], $_POST['email'], (int)$_POST['phone'], $_POST['street'], $_POST['city'], $_POST['state'], (int)$_POST['postal-code']);
-  $session->setName($_POST['name']);
+  $name = $_POST['name'];
+  $name = strRemoveSpeChr($name);
+
+  $street = $_POST['street'];
+  $street = strRemoveSpeChr($street);
+
+  $city = $_POST['city'];
+  $city = strRemoveSpeChr($city);
+
+  $state = $_POST['state'];
+  $state = strRemoveSpeChr($state);
+
+  $user->updateUser($db, $name, $_POST['email'], (int)$_POST['phone'], $street, $city, $state, (int)$_POST['postal-code']);
+  $session->setName($name);
   $session->setEmail($_POST['email']);
   $session->addMessage('updated', 'Profile updated!');
   
